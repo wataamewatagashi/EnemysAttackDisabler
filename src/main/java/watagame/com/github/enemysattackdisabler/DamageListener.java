9998d970ce71cdfe401a2359a723880fab1f57bc
watagame <wataamewatagashi@users.noreply.github.com>
2019-12-07 19:30:55 +0900

    最低限の機能を実装

---
+++ src/main/java/watagame/com/github/enemysattackdisabler/DamageListener.java
@@ -0,0 +1,26 @@
+package watagame.com.github.enemysattackdisabler;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class DamageListener implements Listener {
+
+    private final EnemysAttackDisabler ead;
+
+    public DamageListener(EnemysAttackDisabler ead) {
+        this.ead = ead;
+
+        ead.getServer().getPluginManager().registerEvents(this, ead);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onEnemyAttack(EntityDamageByEntityEvent event) {
+        if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
+            event.setCancelled(true);
+            //ead.getLogger().info("AttackCancelled!!");
+        }
+    }
+}
--- src/main/java/watagame/com/github/enemysattackdisabler/EnemysAttackDisabler.java
+++ src/main/java/watagame/com/github/enemysattackdisabler/EnemysAttackDisabler.java
@@ -1,5 +1,6 @@
 package watagame.com.github.enemysattackdisabler;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnemysAttackDisabler extends JavaPlugin {
@@ -7,11 +8,12 @@ public final class EnemysAttackDisabler extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        new DamageListener(this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        HandlerList.unregisterAll(this);
     }
 }
